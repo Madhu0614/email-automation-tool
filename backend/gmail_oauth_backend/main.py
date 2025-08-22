7cac75ca06f1a4ee8d08df974ae84c4296d3c4e3
Madhu0614 <kotlamadhu0614@gmail.com>
2025-08-23 02:07:25 +0530

    update the upload and campagain pages that is connected to database

--- backend/gmail_oauth_backend/main.py
+++ backend/gmail_oauth_backend/main.py
@@ -62,7 +62,7 @@ app.add_middleware(
     CORSMiddleware,
     allow_origins=[
         "http://localhost:3000", 
-        "https://email-automation-tool-omega.vercel.app/"
+        "https://email-automation-tool-git-main-madhu0614s-projects.vercel.app"
     ],
     allow_credentials=True,
     allow_methods=["*"],
