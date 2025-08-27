644f92c92f858a6d0eee9f3523ab321a069c55ed
Kotla Madhu <kotlamadhu0614@gmail.com>
2025-08-28 01:59:35 +0530

    Update main.py

--- backend/gmail_oauth_backend/main.py
+++ backend/gmail_oauth_backend/main.py
@@ -62,7 +62,8 @@ app.add_middleware(
     CORSMiddleware,
     allow_origins=[
         "http://localhost:3000", 
-        "https://email-automation-tool-7woxip5gg-madhu0614s-projects.vercel.app/"
+        "https://email-automation-tool-7woxip5gg-madhu0614s-projects.vercel.app/",
+        "https://email-automation-tool-1.onrender.com"
     ],
     allow_credentials=True,
     allow_methods=["*"],
